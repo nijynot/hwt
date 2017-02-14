04d6072da0c8b51a4c20089ad1d3dd6649f3e705
nijynot <nijynot@gmail.com>
2017-02-14 15:36:59 +0100

    Complete Task 11

--- haar.py
+++ haar.py
@@ -73,11 +73,64 @@ for x in range(0, 239):
     a[(2*x) + 1] = (np.sqrt(2) / 2)
     Wn = np.concatenate((Wn, [a]), axis=0)
 
+def compressImage(data):
+    #for x in range(0, 478):
+    newData = np.dot(W, data)
+    
+    #for x in range(0, 600):
+    newData = np.dot(newData, np.transpose(Wn))
+    return newData
 
+def arrayToImage(data):
+    return Image.fromarray(data).convert('L')
+    
+    
 #for x in range(0, 478):
-data = np.dot(W, data)
+#data = np.dot(W, data)
 
 #for x in range(0, 600):
-data = np.dot(data, np.transpose(Wn))
+#data = np.dot(data, np.transpose(Wn))
     
-compressedImg = Image.fromarray(data).convert('L')
+#compressedImg = Image.fromarray(data).convert('L')
+
+data = compressImage(data)
+data = compressImage(data)
+
+#arrayToImage(data).show()
+
+Wt = np.transpose(W);
+Wnt = np.transpose(Wn);
+
+
+def decompressImage(data):
+    #for x in range(0, 478):
+    newData = np.dot(Wt, data)
+    
+    #for x in range(0, 600):
+    newData = np.dot(newData, np.transpose(Wnt))
+    return newData
+data = decompressImage(data)
+a = decompressImage(data)
+
+#img = arrayToImage(data)
+
+#averagedData[0][0] = np.mean([data[0][1], data[0][2]]);
+
+def averageImage(data):
+    a2 = np.zeros((600, 478))
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            if j % 2 == 0:
+                a2[i,j], a2[i,j+1] = (data[i,j+1] + data[i,j])/2, (data[i,j+1] - data[i,j])/2
+            else:
+                continue
+
+#for y in range(600):
+#    for x in range(1, 237):
+#        averagedData[y][x] = np.mean([data[y][x], data[y][x + 1]])
+#    for x in range(1, 237):
+#        averagedData[y][236 + x] = (data[y][x + 1] - data[y][x])/2
+
+
+
+
