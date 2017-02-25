be3a37fe609aea49365c65e4e39e28695668eff7
nijynot <nijynot@gmail.com>
2017-02-25 01:20:39 +0100

    Add shape variable

--- haar.py
+++ haar.py
@@ -17,9 +17,6 @@ def loadImage(image):
         print ('No image was found! Input file has to be in the same directory as this code is!')
         return False
 
-def imgToArray(img):
-    return np.asarray(img, dtype="int32")
-
 def trim(arr):
     shape = np.shape(arr)
     if shape[0] % 2 != 0:
@@ -32,57 +29,64 @@ def trim(arr):
     else:
         return arr
 
+def imageToArray(img):
+    return np.asarray(img, dtype="int32")
+
+def arrayToImage(data):
+    return Image.fromarray(data).convert('L')
+
 # Paths
 dir = path.dirname(__file__)
 kvinna = path.join(dir, 'kvinna.jpg')
 
 # Img to array
 img = loadImage(kvinna)
-data = trim(imgToArray(img))
+data = trim(imageToArray(img))
 shape = np.shape(data)
 
-W = np.zeros(600).reshape(1,600)
+
+# Init first rows
+W = np.zeros(shape[0]).reshape(1, shape[0])
 W[0][0] = np.sqrt(2) / 2
 W[0][1] = np.sqrt(2) / 2
 
-Wn = np.zeros(478).reshape(1,478)
+Wn = np.zeros(shape[1]).reshape(1, shape[1])
 Wn[0][0] = np.sqrt(2) / 2
 Wn[0][1] = np.sqrt(2) / 2
 
-for x in range(1, 300):
-    a = np.zeros(600)
+# Add rows to W and Wn
+for x in range(1, int(shape[0]/2)):
+    a = np.zeros(shape[0])
     a[2*x] = (np.sqrt(2) / 2)
     a[(2*x) + 1] = (np.sqrt(2) / 2)
     W = np.concatenate((W, [a]), axis=0)
  
-for x in range(0, 300):
-    a = np.zeros(600)
+for x in range(0, int(shape[0]/2)):
+    a = np.zeros(shape[0])
     a[2*x] = (np.negative(np.sqrt(2) / 2))
     a[(2*x) + 1] = (np.sqrt(2) / 2)
     W = np.concatenate((W, [a]), axis=0)
 
-for x in range(1, 239):
-    a = np.zeros(478)
+for x in range(1, int(shape[1]/2)):
+    a = np.zeros(shape[1])
     a[2*x] = (np.sqrt(2) / 2)
     a[(2*x) + 1] = (np.sqrt(2) / 2)
     Wn = np.concatenate((Wn, [a]), axis=0)
  
-for x in range(0, 239):
-    a = np.zeros(478)
+for x in range(0, int(shape[1]/2)):
+    a = np.zeros(shape[1])
     a[2*x] = (np.negative(np.sqrt(2) / 2))
     a[(2*x) + 1] = (np.sqrt(2) / 2)
     Wn = np.concatenate((Wn, [a]), axis=0)
 
-def compressImage(data):
+def compressImage(image):
+    data = imageToArray(image)
     #for x in range(0, 478):
     newData = np.dot(W, data)
     
     #for x in range(0, 600):
     newData = np.dot(newData, np.transpose(Wn))
-    return newData
-
-def arrayToImage(data):
-    return Image.fromarray(data).convert('L')
+    return arrayToImage(newData)
     
     
 #for x in range(0, 478):
@@ -93,8 +97,8 @@ def arrayToImage(data):
     
 #compressedImg = Image.fromarray(data).convert('L')
 
-data = compressImage(data)
-data = compressImage(data)
+#data = compressImage(data)
+#data = compressImage(data)
 
 #arrayToImage(data).show()
 
@@ -102,22 +106,23 @@ Wt = np.transpose(W);
 Wnt = np.transpose(Wn);
 
 
-def decompressImage(data):
+def decompressImage(image):
+    data = imageToArray(image)
     #for x in range(0, 478):
     newData = np.dot(Wt, data)
     
     #for x in range(0, 600):
     newData = np.dot(newData, np.transpose(Wnt))
-    return newData
-data = decompressImage(data)
-a = decompressImage(data)
+    return arrayToImage(newData)
+    
+#data = decompressImage(data)
+#a = decompressImage(data)
 
 #img = arrayToImage(data)
-
 #averagedData[0][0] = np.mean([data[0][1], data[0][2]]);
 
 def averageImage(data):
-    a2 = np.zeros((600, 478))
+    a2 = np.zeros((shape[0], shape[1]))
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
             if j % 2 == 0:
